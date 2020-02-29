0a9443763da782220c0a2f23335ac9c5f7f4f5de
Tin <trungtin2qn1@gmail.com>
2020-03-01 01:43:31 +0700

    Comment test for ci/cd

--- models/movie_test.go
+++ models/movie_test.go
@@ -1,32 +1,27 @@
 package models
 
-import (
-	"reflect"
-	"testing"
-)
-
-func TestGetMovieByID(t *testing.T) {
-	type args struct {
-		id string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    Movie
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := GetMovieByID(tt.args.id)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("GetMovieByID() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("GetMovieByID() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+// func TestGetMovieByID(t *testing.T) {
+// 	type args struct {
+// 		id string
+// 	}
+// 	tests := []struct {
+// 		name    string
+// 		args    args
+// 		want    Movie
+// 		wantErr bool
+// 	}{
+// 		// TODO: Add test cases.
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			got, err := GetMovieByID(tt.args.id)
+// 			if (err != nil) != tt.wantErr {
+// 				t.Errorf("GetMovieByID() error = %v, wantErr %v", err, tt.wantErr)
+// 				return
+// 			}
+// 			if !reflect.DeepEqual(got, tt.want) {
+// 				t.Errorf("GetMovieByID() = %v, want %v", got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
--- models/theater_test.go
+++ models/theater_test.go
@@ -1,82 +1,74 @@
 package models
 
-import (
-	"database/sql"
-	"reflect"
-	"testing"
+// func recordStats(db *sql.DB, userID, productID int64) (err error) {
+// 	tx, err := db.Begin()
+// 	if err != nil {
+// 		return
+// 	}
 
-	"github.com/DATA-DOG/go-sqlmock"
-)
+// 	defer func() {
+// 		switch err {
+// 		case nil:
+// 			err = tx.Commit()
+// 		default:
+// 			tx.Rollback()
+// 		}
+// 	}()
 
-func recordStats(db *sql.DB, userID, productID int64) (err error) {
-	tx, err := db.Begin()
-	if err != nil {
-		return
-	}
+// 	if _, err = tx.Exec("UPDATE products SET views = views + 1"); err != nil {
+// 		return
+// 	}
+// 	if _, err = tx.Exec("INSERT INTO product_viewers (user_id, product_id) VALUES (?, ?)", userID, productID); err != nil {
+// 		return
+// 	}
+// 	return
+// }
 
-	defer func() {
-		switch err {
-		case nil:
-			err = tx.Commit()
-		default:
-			tx.Rollback()
-		}
-	}()
+// func TestGetTheaterByID(t *testing.T) {
 
-	if _, err = tx.Exec("UPDATE products SET views = views + 1"); err != nil {
-		return
-	}
-	if _, err = tx.Exec("INSERT INTO product_viewers (user_id, product_id) VALUES (?, ?)", userID, productID); err != nil {
-		return
-	}
-	return
-}
+// 	db, mock, err := sqlmock.New()
+// 	if err != nil {
+// 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+// 	}
+// 	defer db.Close()
 
-func TestGetTheaterByID(t *testing.T) {
+// 	mock.ExpectBegin()
+// 	mock.ExpectExec("UPDATE products").WillReturnResult(sqlmock.NewResult(1, 1))
+// 	mock.ExpectExec("INSERT INTO product_viewers").WithArgs(2, 3).WillReturnResult(sqlmock.NewResult(1, 1))
+// 	mock.ExpectCommit()
 
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-	defer db.Close()
+// 	// now we execute our method
+// 	if err = recordStats(db, 2, 3); err != nil {
+// 		t.Errorf("error was not expected while updating stats: %s", err)
+// 	}
 
-	mock.ExpectBegin()
-	mock.ExpectExec("UPDATE products").WillReturnResult(sqlmock.NewResult(1, 1))
-	mock.ExpectExec("INSERT INTO product_viewers").WithArgs(2, 3).WillReturnResult(sqlmock.NewResult(1, 1))
-	mock.ExpectCommit()
+// 	// we make sure that all expectations were met
+// 	if err := mock.ExpectationsWereMet(); err != nil {
+// 		t.Errorf("there were unfulfilled expectations: %s", err)
+// 	}
 
-	// now we execute our method
-	if err = recordStats(db, 2, 3); err != nil {
-		t.Errorf("error was not expected while updating stats: %s", err)
-	}
+// 	type args struct {
+// 		id string
+// 	}
+// 	tests := []struct {
+// 		name    string
+// 		args    args
+// 		want    Theater
+// 		wantErr bool
+// 	}{
+// 		// TODO: Add test cases.
 
-	// we make sure that all expectations were met
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Errorf("there were unfulfilled expectations: %s", err)
-	}
-
-	type args struct {
-		id string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    Theater
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := GetTheaterByID(tt.args.id)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("GetTheaterByID() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("GetTheaterByID() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			got, err := GetTheaterByID(tt.args.id)
+// 			if (err != nil) != tt.wantErr {
+// 				t.Errorf("GetTheaterByID() error = %v, wantErr %v", err, tt.wantErr)
+// 				return
+// 			}
+// 			if !reflect.DeepEqual(got, tt.want) {
+// 				t.Errorf("GetTheaterByID() = %v, want %v", got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
